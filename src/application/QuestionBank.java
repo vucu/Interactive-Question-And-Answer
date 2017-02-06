038fee5bd82fe485870b68516265c9f840631a3e
Vu Cu <cxv@g.ucla.edu>
2017-02-05 17:03:53 -0800

    Add text file config

--- src/application/QuestionBank.java
+++ src/application/QuestionBank.java
@@ -1,34 +1,35 @@
 package application;
 
 import java.util.ArrayList;
+import java.io.*;
 
 public class QuestionBank {
 	ArrayList<String> questions = new ArrayList<String>();
 	ArrayList<String> answers = new ArrayList<String>();
 	int count = 0;
 	int current = -1;
-	
+
 	private void add(String question, String answer) {
 		questions.add(question);
 		answers.add(answer);
 		count++;
 	}
-	
+
 	public String nextQuestion() {
 		if (current>=count-1) {
 			current++;
 			return "No more questions";
 		} 
-		
+
 		current++;
 		return questions.get(current);
 	}
-	
+
 	public String checkAnswer(String answer) {
 		if (current>count-1) {
 			return "No more questions";
 		}
-		
+
 		String a = answers.get(current);
 		if (a.equals(answer)) {
 			return "Correct!";
@@ -36,17 +37,55 @@ public class QuestionBank {
 			return "Incorrect! Correct answer is: " + answers.get(current);
 		}
 	}
-	
+
 	public boolean isOutOfQuestion() {
 		return (current>count-1);
 	}
-	
-	// Add add question & answer here
+
 	QuestionBank() {
-		add("What is the first letter?", "a");
-		add("What eats a mouse?", "cat");
-		add("What do dogs eat?", "bone");
-		add("What year is this?", "2017");
-		add("What is the president name?", "Trump");
+		// The name of the file to open.
+		String fileName = "QuestionBank.txt";
+
+		// This will reference one line at a time
+		String line = null;
+		String q = null;
+		String a = null;
+		boolean isQ = true;
+
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = 
+					new FileReader(fileName);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = 
+					new BufferedReader(fileReader);
+
+			while((line = bufferedReader.readLine()) != null) {
+				if (!line.trim().isEmpty()) {
+					if (isQ) {
+						q = line;
+						isQ = false;
+					} else {
+						a = line;
+						add(q,a);
+						isQ = true;
+					}
+				}
+			}   
+
+			// Always close files.
+			bufferedReader.close();         
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println(
+					"Unable to open file '" + 
+							fileName + "'");                
+		}
+		catch(IOException ex) {
+			System.out.println(
+					"Error reading file '" 
+							+ fileName + "'");                  
+		}
 	}
 }
--- src/application/TimeCounter.java
+++ src/application/TimeCounter.java
@@ -1,24 +1,63 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TimeCounter {
 	int time;
 	int current;
-	
+
 	public int reset() {
 		current = time;
 		return current;
 	}
-	
+
 	public int drop() {
 		current--;
 		if (current<0) current=0;
-		
+
 		return current;
 	}
-	
+
 	// Set the maximum time here
 	TimeCounter() {
-		time = 60;
-		current = time;
+		// The name of the file to open.
+		String fileName = "Timer.txt";
+
+		// This will reference one line at a time
+		String line = null;
+
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = 
+					new FileReader(fileName);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = 
+					new BufferedReader(fileReader);
+
+			while((line = bufferedReader.readLine()) != null) {
+				if (!line.trim().isEmpty()) {
+					time = Integer.parseUnsignedInt(line);
+					current = time;
+					break;
+				}
+			}   
+
+			// Always close files.
+			bufferedReader.close();         
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println(
+					"Unable to open file '" + 
+							fileName + "'");                
+		}
+		catch(IOException ex) {
+			System.out.println(
+					"Error reading file '" 
+							+ fileName + "'");                  
+		}
 	}
 }
